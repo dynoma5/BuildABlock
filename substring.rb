1e25b30d4732a6060d739aee36aa3b2cfe5009fe
David <manakete659@gmail.com>
2017-02-07 15:58:52 -0500

    Updated substring method

--- substring.rb
+++ substring.rb
@@ -1,3 +1,20 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substring (string, dic)
+	list = string.downcase.split()
+	result = Hash.new(0)
+	dic.each do |word| 
+		list.each do |check|
+			if (/#{word}/=~ check) != nil
+			result[word] = result[word] +1
+			end
+		end
+	end
+	print result
+end
+
+
+
+substring("Howdy partner, sit down! How's it going?", dictionary)	
+			
+			
