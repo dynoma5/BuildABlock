60b8f4a2f9c9111959fc65c96235dfd59de52e9a
David <manakete659@gmail.com>
2017-02-06 22:50:57 -0500

    Added new files

---
+++ stock_picker.rb
@@ -0,0 +1,24 @@
+
+def stock_picker (array)
+	optimize = false
+	list = Hash.new()
+	
+	total = array.length
+	0.upto(total-1) do |i|
+		check = array[i]
+		k = i+1
+		while(k <= total-1) do
+			difference = check - array [k]
+			list.store(difference, [i, k])
+			k = k+1
+		end
+	end
+profit = list.keys
+profit.sort!
+max = profit[0]
+print list[max]
+end	
+			
+		
+	
+stock_picker([17,3,6,9,15,8,6,1,10])	
